a5f3f89ad598a26e3b6ec0465faef79b384d3876
Boldeusz <mnpower@poczta.onet.pl>
2021-03-01 14:23:29 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+
+
+
+        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+
+        String[] data = stringData.split(" ");
+        for (String s : data){
+            list.addItem(new Node(s));
+        }
+
+        list.traverse(list.getRoot());
     }
 }
+
--- src/com/company/MyLinkedList.java
+++ src/com/company/MyLinkedList.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class MyLinkedList implements NodeList{
+public class MyLinkedList implements NodeList {
     private ListItem root = null;
 
     public MyLinkedList(ListItem root) {
@@ -14,34 +14,30 @@ public class MyLinkedList implements NodeList{
 
     @Override
     public boolean addItem(ListItem newItem) {
-        if (this.root == null){
+        if (this.root == null) {
             this.root = newItem;
             return true;
         }
         ListItem currentItem = this.root;
-        while (currentItem != null){
+        while (currentItem != null) {
             int comparison = (currentItem.compareTo(newItem));
-            if (comparison < 0){
-                if (currentItem.next() != null){
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
                     currentItem = currentItem.next();
-                }else {
-                    currentItem.setNext(newItem);
-                    newItem.setPrevious(currentItem);
+                } else {
+                    currentItem.setNext(newItem).setPrevious(currentItem);
                     return true;
                 }
-            }else if (comparison > 0){
-                if (currentItem.previous() != null){
-                    currentItem.previous().setNext(newItem);
-                    newItem.setPrevious(currentItem.previous());
-                    newItem.setNext(currentItem);
-                    currentItem.setPrevious(newItem);
-                }else {
-                    newItem.setNext(this.root);
-                    this.root.setPrevious(newItem);
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem).setPrevious(newItem);
+                } else {
+                    newItem.setNext(this.root).setPrevious(newItem);
                     this.root = newItem;
                 }
                 return true;
-            }else {
+            } else {
                 System.out.println(newItem.getValue() + " is already present, not added");
                 return false;
             }
@@ -51,11 +47,40 @@ public class MyLinkedList implements NodeList{
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item != null){
+            System.out.println("Deleting item " + item.getValue());
+        }
+        ListItem currentItem = this.root;
+        while (currentItem != null){
+            int comparison = currentItem.compareTo(item);
+            if (comparison == 0){
+                if (currentItem == this.root){
+                    this.root = currentItem.next();
+                }else {
+                    currentItem.previous().setNext(currentItem.next());
+                    if (currentItem.next() != null){
+                        currentItem.next().setPrevious(currentItem.previous());
+                    }
+                }
+                return true;
+            }else if (comparison < 0){
+                currentItem = currentItem.next();
+            }else {
+                return false;
+            }
+        }
         return false;
     }
 
     @Override
     public void traverse(ListItem item) {
-
+        if (root == null){
+            System.out.println("This list is empty");
+        }else {
+            while (root != null) {
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
     }
 }
