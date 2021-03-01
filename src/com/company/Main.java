bb0d2d6c8b291d4fb3ab274e7b05e13c60aa00ff
Boldeusz <mnpower@poczta.onet.pl>
2021-03-01 19:58:10 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyLinkedList list = new MyLinkedList(null);
-        list.traverse(list.getRoot());
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
 
 
 
@@ -13,31 +13,91 @@ public class Main {
 
         String[] data = stringData.split(" ");
         for (String s : data){
-            list.addItem(new Node(s));
+            tree.addItem(new Node(s));
         }
 
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("3"));
-        list.traverse(list.getRoot());
+        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("3"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("5"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("0"));
+//        tree.removeItem(new Node("4"));
+//        tree.removeItem(new Node("2"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("9"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("8"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("6"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(tree.getRoot());
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(tree.getRoot());
+//        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("5"));
-        list.traverse(list.getRoot());
 
-        list.removeItem(new Node("0"));
-        list.removeItem(new Node("4"));
-        list.removeItem(new Node("2"));
-        list.traverse(list.getRoot());
 
-        list.removeItem(new Node("9"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("8"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("6"));
-        list.traverse(list.getRoot());
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // that is without SearchTree class
+
+//        MyLinkedList tree = new MyLinkedList(null);
+//        tree.traverse(tree.getRoot());
+//
+//
+//
+//        String stringData = "5 7 6 3 4 9 8 0 1 2";
+//
+//        String[] data = stringData.split(" ");
+//        for (String s : data){
+//            tree.addItem(new Node(s));
+//        }
+//
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("3"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("5"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("0"));
+//        tree.removeItem(new Node("4"));
+//        tree.removeItem(new Node("2"));
+//        tree.traverse(tree.getRoot());
+//
+//        tree.removeItem(new Node("9"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("8"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("6"));
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(tree.getRoot());
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(tree.getRoot());
+//        tree.traverse(tree.getRoot());
     }
 }
 
