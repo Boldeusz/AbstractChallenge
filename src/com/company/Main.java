20fb4b891a195ff044f709e6d1c38d086b98b9c6
Boldeusz <mnpower@poczta.onet.pl>
2021-03-01 14:26:30 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,13 +9,34 @@ public class Main {
 
 
 
-        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String stringData = "5 7 6 3 4 9 8 0 1 2";
 
         String[] data = stringData.split(" ");
         for (String s : data){
             list.addItem(new Node(s));
         }
 
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("3"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("5"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("0"));
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node("2"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("9"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("8"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("6"));
+        list.traverse(list.getRoot());
+        list.removeItem(list.getRoot());
+        list.traverse(list.getRoot());
+        list.removeItem(list.getRoot());
         list.traverse(list.getRoot());
     }
 }
